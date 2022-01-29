902437236c4fc0e54ae8c9654bab726542c639d4
zhanfeng <http://zhanfeng@code.shihuo.cn>
2022-01-29 16:51:16 +0800

    feat:增加 bundle 阈值调节配置文件

--- ams-lib/src/main/java/com/shyii/ams_lib/AMSMonitor.kt
+++ ams-lib/src/main/java/com/shyii/ams_lib/AMSMonitor.kt
@@ -16,14 +16,19 @@ import java.lang.reflect.Method
 object AMSMonitor {
 
     private const val TAG = "ActivityManagerHook"
+    private const val MAX_BUNDLE_SIZE = 512 * 1024
+    private var bundleSizeThreshold = (80 * MAX_BUNDLE_SIZE) / 100
 
-    fun init(app: Application?) {
+    fun init(app: Application) {
+        initBundleSizeThreshold(app)
         hookStartActivity()
-        app?.registerActivityLifecycleCallbacks(object :
+        app.registerActivityLifecycleCallbacks(object :
             Application.ActivityLifecycleCallbacks by noOpDelegate() {
             override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
                 val size = outState.sizeAsParcel()
-                Log.e(TAG, "onActivitySaveInstanceState >> bundle.sizeAsParcel:$size")
+                if (size > bundleSizeThreshold) {
+                    Log.e(TAG, "onActivitySaveInstanceState >> bundle.sizeAsParcel:$size")
+                }
             }
         })
     }
@@ -35,13 +40,24 @@ object AMSMonitor {
                 for (index in args.indices) {
                     if (args[index] is Intent) {
                         val size = (args[index] as Intent).extras?.sizeAsParcel() ?: 0
-                        Log.e(TAG, "startActivity >> bundle.sizeAsParcel:$size")
+                        if (size > bundleSizeThreshold) {
+                            Log.e(TAG, "startActivity >> bundle.sizeAsParcel:$size")
+                        }
                         break
                     }
                 }
             }
         }
         ActivityManagerServiceHook.hookAMS()
+    }
+
+    private fun initBundleSizeThreshold(app: Application) {
+        var bundleThresholdPercent =
+            app.resources.getInteger(R.integer.bundle_size_threshold_percent)
+        if (bundleThresholdPercent < 0 || bundleThresholdPercent > 100) {
+            bundleThresholdPercent = 80
+        }
+        bundleSizeThreshold = (bundleThresholdPercent * MAX_BUNDLE_SIZE) / 100
     }
 
 }
